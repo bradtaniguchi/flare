7198c633d6837bf30a462a33d99371c21b2f035c
brad taniguchi <bradtaniguchi@gmail.com>
2019-01-16 21:47:34 -0800

    Added redirects to dashboard-actions
    - now we redirect the user to the correct pages, only the card form
    redirect works.

--- src/app/modules/dashboard/dashboard-actions/dashboard-actions.component.ts
+++ src/app/modules/dashboard/dashboard-actions/dashboard-actions.component.ts
@@ -4,8 +4,22 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-dashboard-actions',
   template: `
     <div fxLayout="row" fxLayoutAlign="center">
-      <button mat-button type="button" color="primary">Create Deck</button>
-      <button mat-button type="button" color="primary">Create Card</button>
+      <button
+        mat-button
+        type="button"
+        color="primary"
+        routerLink="/decks/create"
+      >
+        Create Deck
+      </button>
+      <button
+        mat-button
+        type="button"
+        color="primary"
+        routerLink="/cards/create"
+      >
+        Create Card
+      </button>
     </div>
   `,
   styles: [],
--- src/app/modules/dashboard/dashboard-actions/dashboard-actions.module.ts
+++ src/app/modules/dashboard/dashboard-actions/dashboard-actions.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardActionsComponent } from './dashboard-actions.component';
 import { MatButtonModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [DashboardActionsComponent],
   imports: [
     CommonModule,
-
+    RouterModule,
     // angular material
     FlexLayoutModule,
     MatButtonModule
