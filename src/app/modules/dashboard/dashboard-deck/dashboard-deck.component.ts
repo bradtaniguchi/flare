ef3b4d3158466af8e861baac53b03bb3d543e98d
brad taniguchi <bradtaniguchi@gmail.com>
2019-01-31 18:56:38 -0800

    Started making deck cards cooler!

--- src/app/modules/dashboard/dashboard-deck/dashboard-deck.component.ts
+++ src/app/modules/dashboard/dashboard-deck/dashboard-deck.component.ts
@@ -11,14 +11,22 @@ import { Deck } from 'src/app/models/deck';
 @Component({
   selector: 'app-dashboard-deck',
   template: `
-    <mat-card class="margin-top-bottom">
-      <mat-card-title> {{ deck.name }} </mat-card-title>
+    <mat-card
+      class="margin-top-bottom clickable"
+      (click)="study.emit(deck)"
+      matRipple
+    >
+      <mat-card-header>
+        <mat-icon mat-card-avatar class="center-icon">library_books</mat-icon>
+        <mat-card-title> {{ deck.name }} </mat-card-title>
+      </mat-card-header>
       <mat-card-content>
         <p>{{ deck.name }}</p>
         <p># of cards: {{ tempCardCount() }}</p>
+        <mat-divider></mat-divider>
       </mat-card-content>
-      <mat-card-actions align="end">
-        <button
+      <mat-card-actions fxLayout="row" fxLayoutAlign="space-between">
+        <!--<button
           type="button"
           mat-button
           color="warn"
@@ -26,17 +34,28 @@ import { Deck } from 'src/app/models/deck';
           (click)="remove.emit(deck)"
         >
           Delete
-        </button>
-        <button type="button" mat-button (click)="viewCards.emit(deck)">
-          Cards
-        </button>
+        </button>-->
+        <div>
+          <button
+            type="button"
+            mat-button
+            color="primary"
+            (click)="study.emit(deck)"
+          >
+            Study
+          </button>
+          <button type="button" mat-button (click)="viewCards.emit(deck)">
+            Cards
+          </button>
+        </div>
         <button
           type="button"
-          mat-button
-          color="primary"
-          (click)="study.emit(deck)"
+          mat-icon-button
+          aria-label="More Options"
+          fxFlexAlign="end"
         >
-          Study
+          <!-- TODO: make menu appear -->
+          <mat-icon>more_vert</mat-icon>
         </button>
       </mat-card-actions>
     </mat-card>
--- src/app/modules/dashboard/dashboard-deck/dashboard-deck.module.ts
+++ src/app/modules/dashboard/dashboard-deck/dashboard-deck.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardDeckComponent } from './dashboard-deck.component';
-import { MatCardModule, MatButtonModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
+import { MatRippleModule } from '@angular/material/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [DashboardDeckComponent],
   imports: [
     CommonModule,
     // angular material
+    FlexLayoutModule,
+    MatRippleModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   exports: [DashboardDeckComponent]
 })
