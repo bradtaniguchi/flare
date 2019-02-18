4f06519ae1421d8b2bb6146524b4735640f047b7
brad taniguchi <bradtaniguchi>
2019-02-18 22:09:34 +0000

    Updated the study-action buttons
    - this button does not do anything right now, but will in the future.

--- src/app/modules/dashboard/dashboard-study-actions/dashboard-study-actions.component.ts
+++ src/app/modules/dashboard/dashboard-study-actions/dashboard-study-actions.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AppState } from 'src/app/app-store/app-state';
+import { Store } from '@ngrx/store';
+import { Notify } from 'src/app/app-store/notify/notify.actions';
 
 @Component({
   selector: 'app-dashboard-study-actions',
   template: `
-    <mat-card> Study actions </mat-card>
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title> Start Studying </mat-card-title>
+      </mat-card-header>
+      <mat-card-content fxLayoutAlign="center" fxLayoutGap="16px">
+        <button
+          mat-raised-button
+          type="primary"
+          color="primary"
+          class="large"
+          (click)="noop()"
+        >
+          Study Random Deck
+        </button>
+      </mat-card-content>
+    </mat-card>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardStudyActionsComponent implements OnInit {
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {}
+
+  noop() {
+    this.store.dispatch(
+      new Notify({
+        message: 'Sorry feature is not setup yet'
+      })
+    );
+  }
 }
--- src/app/modules/dashboard/dashboard-study-actions/dashboard-study-actions.module.ts
+++ src/app/modules/dashboard/dashboard-study-actions/dashboard-study-actions.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardStudyActionsComponent } from './dashboard-study-actions.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [DashboardStudyActionsComponent],
-  imports: [CommonModule, FlexLayoutModule, MatCardModule],
+  imports: [
+    CommonModule,
+    // angular material
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   exports: [DashboardStudyActionsComponent]
 })
 export class DashboardStudyActionsModule {}
